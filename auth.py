c61d2f57eaea3c959b1207df619cf3e763443cb8
Sanyamjain6262 <145764192+Sanyamjain6262@users.noreply.github.com>
2025-04-30 22:57:46 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,11 +1,21 @@
 def login():
-    print("🔐 BANK LOGIN")
+    print("BANK LOGIN")
     attempts = 3
+    
+    with open("password_username.txt", "r") as f:
+        valid_credentials = {}
+        for line in f:
+            if ":" in line and not line.startswith("#"):
+                username, password = line.strip().split(":")
+                valid_credentials[username] = password
+
     while attempts > 0:
         username = input("Username: ")
         password = input("Password: ")
-        if username == "admin" and password == "bank123":
+        
+        if username in valid_credentials and valid_credentials[username] == password:
             return True
         attempts -= 1
-        print(f"❌ Invalid credentials. {attempts} attempts left")
+        print(f"Invalid credentials. {attempts} attempts left")
+    
     return False
